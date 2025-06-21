7796ae32ad60e42172cb0438725e64c11f17bece
imas8888 <sami.khan@toukanlabs.com>
2025-06-21 13:06:07 +0100

    Fixing sanity config

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: 'r7wwqdic',
+  dataset: 'production',
   useCdn: false,
   apiVersion: '2024-03-19',
   perspective: "published",
